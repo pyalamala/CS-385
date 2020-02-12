202557d20dc9f0cb8525f776ec1cfcaabde574c0
Robert Schaedler III <robertschaedler3@gmail.com>
2020-02-12 16:32:23 -0500

    add programming assignment2

---
+++ unique-letters/unique.cpp
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Name        : unique.cpp
+ * Author      : Robert Schaedler III
+ * Date        : Feb. 12 2020
+ * Description : Determining uniqueness of chars with int as bit vector.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+#include <cctype>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool is_all_lowercase(const string &s) {
+    for (const auto &c : s) {
+        if (c < 'a' || c > 'z') {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool all_unique_letters(const string &s) {
+    unsigned int bit_set = 0;
+    for (const auto &c : s) {
+        unsigned int char_setter = 1 << (c - 'a');
+        if (char_setter & bit_set) {
+            return false;
+        }
+        bit_set |= char_setter;
+    }
+
+    return true;
+}
+
+int main(int argc, char *const argv[]) {
+    if (argc > 2 || argc < 2) {
+        cerr << "Usage: " << argv[0] << " <string>" << endl;
+        return 1;
+    }
+
+    istringstream iss;
+    string s;
+
+    iss.str(argv[1]);
+    if (!(iss >> s) | !(is_all_lowercase(s))) {
+        cerr << "Error: String must contain only lowercase letters." << endl;
+        return 1;
+    }
+    iss.clear();
+
+    if (!all_unique_letters(s)) {
+        cout << "Duplicate letters found." << endl;
+    } else {
+        cout << "All letters are unique." << endl;
+    }
+
+    return 1;
+}
