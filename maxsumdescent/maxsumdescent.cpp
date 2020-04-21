d4800b939a8296f14b76f8e9979aa377797e3813
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-21 09:18:50 -0400

    add template

---
+++ maxsumdescent/maxsumdescent.cpp
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Name        : maxsumdescent.cpp
+ * Author      : 
+ * Version     : 1.0
+ * Date        : 
+ * Description : Dynamic programming solution to max sum descent problem.
+ * Pledge      :
+ ******************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const char DELIMITER = ' ';
+
+int **values, // This is your 2D array of values, read from the file.
+    **sums;   // This is your 2D array of partial sums, used in DP.
+
+int num_rows; // num_rows tells you how many rows the 2D array has.
+              // The first row has 1 column, the second row has 2 columns, and
+              // so on...
+
+/**
+ * Displays the 2D array of values read from the file in the format of a table.
+ */
+void display_table() {
+    // TODO
+}
+
+/**
+ * Returns the maximum sum possible in the triangle of values.
+ * By starting at the top of the triangle and moving to adjacent numbers on the
+ * row below, this function uses dynamic programming to build up another table
+ * of partial sums.
+ */
+int compute_max_sum() {
+    // TODO
+
+    // Populate the 2D array of partial sums. It should still work even if
+    // num_rows is 0 (i.e. the file was blank).
+
+    // Loop over the last row to find the max sum.
+
+    // Return the max sum.
+    return 0;
+}
+
+/**
+ * Returns a vector of ints with the values from the top to the bottom of the
+ * triangle that comprise the maximum sum.
+ */
+vector<int> backtrack_solution() {
+    vector<int> solution;
+    // TODO
+
+    return solution;
+}
+
+/**
+ * Reads the contents of the file into the global 2D array 'values'. If
+ * successful, the function also allocates memory for the 2D array 'sums'.
+ */
+bool load_values_from_file(const string &filename) {
+    ifstream input_file(filename.c_str());
+    if (!input_file) {
+        cerr << "Error: Cannot open file '" << filename << "'." << endl;
+        return false;
+    }
+    input_file.exceptions(ifstream::badbit);
+    string line;
+    vector<string> data;
+    try {
+        while (getline(input_file, line)) {
+            data.push_back(line);
+        }
+        input_file.close();
+    } catch (const ifstream::failure &f) {
+        cerr << "Error: An I/O error occurred reading '" << filename << "'.";
+        return false;
+    }
+    
+    // TODO
+
+    return true;
+}
+
+/**
+ * Frees up the memory allocated for the 2D array of values and the 2D array of
+ * partial sums.
+ */
+void cleanup() {
+    // TODO
+}
+
+int main(int argc, char * const argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
+    string filename(argv[1]);
+    if (!load_values_from_file(filename)) {
+        return 1;
+    }
+
+    // TODO
+
+    return 0;
+}
